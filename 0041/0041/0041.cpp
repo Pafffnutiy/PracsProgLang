419bc51ee8fd4b45e8ecf60a04aa0c3949f64673
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:24:03 +0300

    Uploaded Task 0041

---
+++ 0041/0041/0041.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a, b, c;
+    if (!(std::cin >> a >> b >> c) || (((a <= 1) || (a >= 3)) && ((b <= 1) || (b >= 3)) && ((c <= 1) || (c >= 3)))) { return 1; }
+    if ((a > 1) && (a < 3)) { std::cout << a << std::endl; };
+    if ((b > 1) && (b < 3)) { std::cout << b << std::endl; };
+    if ((c > 1) && (c < 3)) { std::cout << c << std::endl; };
+}
