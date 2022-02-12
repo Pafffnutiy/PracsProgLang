22dbfe5c75ad2b401669db888af6c9495e83e685
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:24:33 +0300

    Uploaded Task 0024

---
+++ 0042/0042/0042.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    double x, y, p, t;
+    if (!(std::cin >> x >> y) || (x == y)) { return 1; }
+    p = (x + y) / 2;
+    t = 2 * x * y;
+    if (x < y) { std::cout << p << std::endl << t; };
+    if (y < x) { std::cout << t<< std::endl << p; };
+    return 0;
+ }
