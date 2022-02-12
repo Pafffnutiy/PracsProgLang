7dfe1c3165ab22b002e5c7be43a35a508549f945
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:26:09 +0300

    Uploaded Task 0046

---
+++ 0046/0046/0046.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    double x, y;
+    if (!(std::cin >> x >> y)) { return 1; }
+    if ((x < 0) && (y < 0)) { std::cout << abs(x) << std::endl << abs(y); }
+    if (x * y < 0) { std::cout << x + 0.5 << std::endl << y + 0.5; }
+    if ((x >= 0) && (y >= 0) && ((x < 0.5) || (x > 2)) && ((y < 0.5) || (y > 2))) { std::cout << x / 10 << std::endl << y / 10; }
+    if ((x >= 0.5) && (x <= 2) || (y >= 0.5) && (y <= 2)) { std::cout << x << std::endl << y; }
+    return 0;
+}
