0aeb0cfc7e7107cae95715670a5e80fc26e227aa
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:25:10 +0300

    Uploaded Task 0045

---
+++ 0045/0045/0045.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    double a, b, c, d;
+    if (!(std::cin >> a >> b >> c >> d)) { return 1; }
+    if ((a <= b) && (b <= c) && (c <= d)) { std::cout << d << std::endl << d << std::endl << d << std::endl << d; };
+    if ((a > c) && (c > b) && (b > d)) { std::cout << a << std::endl << b << std::endl << c << std::endl << d; };
+    if (!((a <= b) && (b <= c) && (c <= d)) && !((a > c) && (c > b) && (b > d))) { std::cout << a * a << std::endl << b * b << std::endl << c * c << std::endl << d * d; };
+    return 0;
+}
