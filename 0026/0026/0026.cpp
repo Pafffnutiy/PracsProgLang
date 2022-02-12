7015ef054529734384fe0aa27c68a6b0838860d8
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:23:23 +0300

    Uploaded Task 0026

---
+++ 0026/0026/0026.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double R = 13.7;
+    double alpha;
+    if (!(std::cin >> alpha) || (alpha < 0) || (alpha > 2 * 3.14)) { return 1; }
+    double S;
+    S = 0.5 * alpha * pow(R, 2);
+    std::cout << S;
+    return 0;
+}
