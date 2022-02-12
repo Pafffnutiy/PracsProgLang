bb72cc5fe40a860a69a8fa6e4a4561f565952430
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:28:02 +0300

    Uploaded Task 0066

---
+++ 0066/0066/0066.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double k, m, x, y, z;
+    if (!(std::cin >> k >> m >> x >> y >> z) || (floor(k) != k) || (floor(m) != m)) { return 1; }
+    if (k < m * m) { std::cout << abs(x) << std::endl << y - 0.5 << std::endl << z - 0.5; }
+    if (k = m * m) { std::cout << x - 0.5 << std::endl << abs(y) << std::endl << z - 0.5; }
+    if (k > m * m) { std::cout << x - 0.5 << std::endl << y - 0.5 << std::endl << abs(z); }
+    return 0;
+ }
