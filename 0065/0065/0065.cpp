5c4a75d7f0b9a9c6d64ba8f8c346974bd75135da
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:28:55 +0300

    Uploaded Task 0065

---
+++ 0065/0065/0065.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double n, b;
+    if (!(std::cin >> n >> b) || (n > 99) || (floor(n) != n) || (n <= 0)) { return 1; };
+    int a = static_cast<int>(n);
+    if (pow(a % 10 + a / 10 % 10, 3) == pow(b, 2)) { std::cout << "YES"; }
+    else { std::cout << "NO"; };
+    return 0;
+}
