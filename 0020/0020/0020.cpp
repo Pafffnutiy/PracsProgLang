2aa40f55b0e1ca67807852879a9ed864ebfd295b
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:30:15 +0300

    Uploaded Task 0020

---
+++ 0020/0020/0020.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+int main() {
+    double a, d;
+    double n;
+    if ((!(std::cin >> a >> d >> n)) || (n <= 0) || (floor(n) != n)) { return 1; }
+    double ans;
+    ans = ((2 * a + d * (n - 1)) / 2) * n;
+    std::cout << ans;
+    return 0;
+}
