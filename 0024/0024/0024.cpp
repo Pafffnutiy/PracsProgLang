8158412cf1c1f1bd5d3efb8ece708379d92dbbcb
Pafnuity <65403727+Pafnuity@users.noreply.github.com>
2022-02-12 15:20:25 +0300

    Uploaded Task 0024

---
+++ 0024/0024/0024.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double x1, y1, x2, y2;
+    if (!(std::cin >> x1 >> y1 >> x2 >> y2)) { return 1; }
+    double ans = sqrt((pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+    std::cout << ans;
+}
